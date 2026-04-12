7f86ee716002f339f3f3b726abfe011bf9b75fa3
Khanyantandathu <202608067@spu.ac.za>
2026-04-12 23:51:12 +0200

    final Atelier

---
+++ home.js
@@ -0,0 +1,15 @@
+const hamburger = document.querySelector('.hamburger');
+        const navMenu = document.querySelector('nav ul');
+
+        hamburger.addEventListener('click', () => {
+            hamburger.classList.toggle('active');
+            navMenu.classList.toggle('active');
+        });
+
+        // Close menu when clicking on a link
+        document.querySelectorAll('nav ul li a').forEach(link => {
+            link.addEventListener('click', () => {
+                hamburger.classList.remove('active');
+                navMenu.classList.remove('active');
+            });
+        });
